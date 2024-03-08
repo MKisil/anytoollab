34cf88dd24cf676f00f188aaafba75bce3042ec5
Mikhaylo Kisil <mihajlokisil20@gmail.com>
2024-03-08 23:49:53 +0200

    Access to media folder for celery and celery beats

--- entrypoint_celery.sh
+++ entrypoint_celery.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 chown -R nobody:nogroup /usr/src/app/
+mkdir -p /usr/src/app/media/
+chmod -R u+rwX,go+rX,go-w /usr/src/app/media/
 
 celery --workdir=/usr/src/app -A config worker -l INFO --uid=nobody --gid=nogroup
--- entrypoint_celery_beats.sh
+++ entrypoint_celery_beats.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-python manage.py migrate --noinput --settings=config.settings.local
+python manage.py migrate --noinput --settings=config.settings.production
 
 chown -R nobody:nogroup /usr/src/app/
+mkdir -p /usr/src/app/media/
+chmod -R u+rwX,go+rX,go-w /usr/src/app/media/
 
 celery --workdir=/usr/src/app -A config beat -l INFO --scheduler django_celery_beat.schedulers:DatabaseScheduler --uid=nobody --gid=nogroup
