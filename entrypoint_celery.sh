6ea649ec6b4893d0147860d9ef16e5912882b9b9
Mikhaylo Kisil <mihajlokisil20@gmail.com>
2024-03-09 21:27:46 +0200

    Removed access permissions

--- entrypoint_celery.sh
+++ entrypoint_celery.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-chown -R nobody:nogroup /usr/src/app/
-mkdir -p /usr/src/app/media/
-chmod -R u+rwX,go+rX,go-w /usr/src/app/media/
-
-celery --workdir=/usr/src/app -A config worker -l INFO --uid=nobody --gid=nogroup
+celery --workdir=/usr/src/app -A config worker -l INFO
--- entrypoint_celery_beats.sh
+++ entrypoint_celery_beats.sh
@@ -2,8 +2,4 @@
 
 python manage.py migrate --noinput --settings=config.settings.production
 
-chown -R nobody:nogroup /usr/src/app/
-mkdir -p /usr/src/app/media/
-chmod -R u+rwX,go+rX,go-w /usr/src/app/media/
-
-celery --workdir=/usr/src/app -A config beat -l INFO --scheduler django_celery_beat.schedulers:DatabaseScheduler --uid=nobody --gid=nogroup
+exec celery --workdir=/usr/src/app -A config beat -l INFO --scheduler django_celery_beat.schedulers:DatabaseScheduler
